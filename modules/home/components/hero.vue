7e92907675893e16443acbdd6759ed296939432f
Artur <111369356+MikroTik2@users.noreply.github.com>
2025-02-08 20:26:25 +0200

    fix: fixed hydration issue in the logo

--- modules/home/components/hero.vue
+++ modules/home/components/hero.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useWindowSize } from '@vueuse/core';
 import { Motion } from 'motion-v';
+
+const { width } = useWindowSize();
+const isVisible = computed(() => width.value >= 768);
 </script>
 
 <template>
@@ -11,12 +15,22 @@ import { Motion } from 'motion-v';
             :in-view="{ opacity: 1, y: 0, filter: 'blur(0px)' }"
             :transition="{ delay: 0.3, duration: 0.8, ease: 'easeInOut' }"
         >
-            <div class="flex flex-col h-full items-center justify-center">
-                <ParticleImage
-                    image-src="/images/particle.png"
-                    :responsive-width="true"
-                />
-            </div>
+            <ClientOnly>
+                <div v-if="isVisible" class="flex flex-col h-full items-center justify-center">
+                    <ParticleImage
+                        image-src="/images/particle.png"
+                        :responsive-width="true"
+                    />
+                </div>
+            </ClientOnly>
+            <NuxtLink to="/" class="inline-flex items-center rounded-full bg-muted px-4 py-1.5 text-sm font-medium hover:cursor-pointer md:hidden">
+                <LucideRocket class="!h-[16px] !w-[16px]" />
+
+                <Separator orientation="vertical" class="shrink-0 bg-border w-px mx-2 h-4 dark:bg-gray-600" />
+
+                <span class="underline-offset-4 hover:underline">Get Started</span>
+                <LucideArrowRight class="!h-[16px] !w-[16px] ml-1" />
+            </NuxtLink>
 
             <h1 class="text-center text-3xl font-bold md:text-7xl dark:text-white">
                 File Compression Made Simple.
